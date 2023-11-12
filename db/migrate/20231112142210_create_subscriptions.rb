5adc2d8fa34bbfade190a6e247326662aa00a704
michelfi <michel.figueres@gmail.com>
2023-11-12 18:52:39 +0100

    oui

--- db/migrate/20231112142210_create_subscriptions.rb
+++ db/migrate/20231112142210_create_subscriptions.rb
@@ -2,7 +2,6 @@ class CreateSubscriptions < ActiveRecord::Migration[7.0]
   def change
     create_table :subscriptions do |t|
       t.references :subscriber, null: false, foreign_key: true
-      t.references :newsletter, null: false, foreign_key: true
 
       t.timestamps
     end
