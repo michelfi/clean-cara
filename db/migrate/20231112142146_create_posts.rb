fb7beb022f48f3d9e7ab7e91d2fce9198664ade1
michelfi <michel.figueres@gmail.com>
2023-11-12 18:50:25 +0100

    modifi

--- db/migrate/20231112142146_create_posts.rb
+++ db/migrate/20231112142146_create_posts.rb
@@ -4,7 +4,6 @@ class CreatePosts < ActiveRecord::Migration[7.0]
       t.string :title
       t.string :category
       t.string :content
-      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
